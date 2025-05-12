4498d866345f22960a75e223ac995aecdd82add1
aieghbal <aieghbal.me@gmail.com>
2025-05-12 16:36:18 +0330

    Create controler that use PostRepositoryInterface

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use App\Repositories\PostRepository;
 use App\Repositories\PostRepositoryInterface;
 
 class PostController extends Controller
 {
-    public function index(PostRepositoryInterface $postRepository)
+    protected $postRepo;
+
+    public function __construct(PostRepositoryInterface $postRepo)
     {
-        return $postRepository->all();
+        $this->postRepo = $postRepo;
+    }
+
+    public function index()
+    {
+        return response()->json($this->postRepo->all());
     }
 }
