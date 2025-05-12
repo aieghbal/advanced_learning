dd61a43e4ef744277542adeefccc2737b2f63e5c
aieghbal <aieghbal.me@gmail.com>
2025-05-12 15:07:45 +0330

    Dependency Injection And Interface

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\PostRepository;
+use App\Repositories\PostRepositoryInterface;
 
 class PostController extends Controller
 {
-    public function index(PostRepository $postRepository)
+    public function index(PostRepositoryInterface $postRepository)
     {
         return $postRepository->all();
     }
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -3,7 +3,7 @@ namespace App\Repositories;
 
 use App\Models\Post;
 
-class PostRepository
+class PostRepository implements PostRepositoryInterfacey
 {
     public function all()
     {
