9710a443a91a0f9331fb4aff7e45187c6c2a01bd
aieghbal <aieghbal.me@gmail.com>
2025-05-12 15:20:37 +0330

    Add other method into Repository

--- app/Repositories/PostRepositoryInterface.php
+++ app/Repositories/PostRepositoryInterface.php
@@ -4,5 +4,9 @@ namespace App\Repositories;
 interface PostRepositoryInterface
 {
     public function all();
+    public function find($id);
+    public function create(array $data);
+    public function update($id, array $data);
+    public function delete($id);
 }
 ?>
